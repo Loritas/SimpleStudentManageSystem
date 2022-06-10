8f350667bcab4657b0091f3918e7959f822a54fb
Loritas <2223292817@qq.com>
2022-06-10 12:54:42 +0800

    优化了文件的读取，添加了配置文件

--- utils/config.h
+++ utils/config.h
@@ -2,7 +2,7 @@
  * @Author: Loritas 2223292817@qq.com
  * @Date: 2022-06-09 20:29:44
  * @LastEditors: Loritas 2223292817@qq.com
- * @LastEditTime: 2022-06-10 01:55:05
+ * @LastEditTime: 2022-06-10 12:53:07
  * @FilePath: /SimpleStudentManageSystem/utils/config.h
  * @Description: 
  * Copyright (c) 2022 by Loritas 2223292817@qq.com, All Rights Reserved. 
@@ -11,12 +11,12 @@
 #define __CONFIG_H__
 #include <unordered_map>
 #include <cstring>
-#include <stdlib.h>
-#include <iostream>
 #include "writer.h"
 #include "../src/studentList.h"
 
-const static std::string CONFIG_PATH = "../data/config.ini";
+static StudentList sList;
+
+const static std::string CONFIG_PATH = "./SimpleStudentManageSystem/data/config.ini";
 const static int BUF_SIZE = 1024;
 
 // 我实在没搞明白它的工作路径为什么在这个文件夹外边……
@@ -25,12 +25,27 @@ static std::string DATA_FILE_NAME = "data.ini";
 
 typedef void(Student::*studentSetFunc)(std::string);
 static std::unordered_map<std::string, studentSetFunc> studentSetAbtMap;
+static std::unordered_set<std::string> configSet;
+static std::unordered_map<std::string, std::string> configMap;
+
+const static void init();
+const static void saveStudentData(StudentList&);
+const static StudentList& readStudentData();
+const static void readConfig();
+const static void setConfig();
 
 const static void init()
 {
-    studentSetAbtMap["Name"] = &Student::setName;
-    studentSetAbtMap["JavaScore"] = &Student::setJavaScore;
-    studentSetAbtMap["CppScore"] = &Student::setCppScore;
+    studentSetAbtMap["Stu_Name"] = &Student::setName;
+    studentSetAbtMap["Stu_JavaScore"] = &Student::setJavaScore;
+    studentSetAbtMap["Stu_CppScore"] = &Student::setCppScore;
+
+    configSet.insert("sys_data_path");
+    configSet.insert("sys_data_file_name");
+
+    readConfig();
+    setConfig();
+    
 }
 
 const static void saveStudentData(StudentList& sList)
@@ -47,36 +62,38 @@ const static void saveStudentData(StudentList& sList)
     ofs << "# it can only do a simple check" << std::endl;
     ofs << "# use '#' as an annotation" << std::endl;
     ofs << "# each sequence end by ';'" << std::endl;
-    ofs << "# usage: name=loritas;" << std::endl;
-    ofs << "# do not leave a ' ' besides '='" << std::endl;
+    ofs << "# usage: Stu_Name=loritas;" << std::endl;
+    ofs << "# do not leave a ' ' besides '=' or the head of each sequence" << std::endl;
 
     std::vector<Student> list = sList.getList();
     for(std::vector<Student>::iterator iter= list.begin(); iter != list.end(); iter++)
     {       
         writeHead(iter->getSid(), ofs);
-        writeData("Name", iter->getName(), ofs);
-        writeData("JavaScore", iter->getJavaScore(), ofs);
-        writeData("CppSCore", iter->getCppScore(), ofs);
+        writeData("Stu_Name", iter->getName(), ofs);
+        writeData("Stu_JavaScore", iter->getJavaScore(), ofs);
+        writeData("Stu_CppScore", iter->getCppScore(), ofs);
     }
     ofs.close();
 }
 
-const static StudentList readStudentData()
+const static StudentList& readStudentData()
 {
-    StudentList sList;
+    Student curStu;
 
     std::ifstream ifs;
+    // "data.ini"
     ifs.open(DATA_PATH+DATA_FILE_NAME, std::ios::in);
     if (!ifs.is_open())
     {
         // TODO err_msg
     }
-
-    char buf[BUF_SIZE] = {0};
-    Student curStu;
-    while (ifs >> buf)
+    
+    char buf[BUF_SIZE];
+    
+    while (!ifs.eof())
     {
-        if (buf[0] == '\n' || buf[0] == '#')
+        ifs.getline(buf, BUF_SIZE);
+        if (buf[0] == '\n' || buf[0] == '#' || buf[0] == '\0')
         {
             continue;
         }
@@ -114,6 +131,69 @@ const static StudentList readStudentData()
 
     ifs.close();
     return sList;
+}
+
+
+const static void readConfig()
+{
+    std::ifstream ifs;
+    ifs.open(CONFIG_PATH, std::ios::in);
+    if (!ifs.is_open())
+    {
+        // TODO err_msg
+    }
+
+    char buf[BUF_SIZE] = {0};
+    while (!ifs.eof())
+    {
+        ifs.getline(buf, BUF_SIZE);
+        if (buf[0] == '\n' || buf[0] == '#' || buf[0] == '\0')
+        {
+            continue;
+        }
+
+        if (buf[0] == '[')
+        {
+            char *savePtr = NULL;
+            char *attri = buf+1;
+            attri = strtok_r(attri, "]", &savePtr);
+            if (strcmp(attri, "Config") != 0)
+            {
+                break;
+                // TODO err_msg
+            }
+        }
+        else 
+        {
+            char *savePtr = NULL;
+            char *attri = strtok_r(buf, "=", &savePtr);
+            if (configSet.find(attri) == configSet.end())
+            {
+                continue;
+            }
+            char *key = attri;
+            int len = strlen(attri);
+            attri = strtok_r(buf + len + 1, ";", &savePtr);
+            configMap[key] = attri;
+        }
+    }
+
+    ifs.close();
+}
+
+const static void setConfig()
+{
+    auto pair = configMap.find("sys_data_path");
+    if (pair != configMap.end())
+    {
+        DATA_PATH = pair->second;
+    }
+
+    pair = configMap.find("sys_data_file_name");
+    if (pair != configMap.end())
+    {
+        DATA_PATH = pair->second;
+    }
 }
 
 #endif
